3c399f9cbb2ff3c5e6c31be5773fddaf2b0dc3d2
Marissa Zhong <mwz2101@columbia.edu>
2018-08-06 16:55:53 +0000

    automatically backed up by learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -10,7 +10,7 @@ export default class ChromeBoisDomain extends Component {
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
-     return drawChromeBoiAtCoords(x,y);
+     return drawChromeBoiAtCoords(event.clientX,event.clientY);
   }
 
    handleClick = () => {
